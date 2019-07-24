7c4788127f56059ad493ad26ab99a0069dabe109
Rane <github@blasph.me>
2019-07-23 17:53:25 -0700

    starting the hair and adjusting gradient colors

--- src/canvases/Freddie.js
+++ src/canvases/Freddie.js
@@ -2,14 +2,30 @@ import React from "react";
 import styled from "styled-components";
 
 const Freddie = _ => {
-  return <Background />;
+  return (
+    <Background>
+      <Hair1 />
+    </Background>
+  );
 };
 
 export default Freddie;
 
 const Background = styled.div`
+  poisition: relative;
   width: 740px;
   height: 740px;
   margin: 25px auto;
-  background-image: linear-gradient(#b5cfe8, #7892ab 10%, #cae4fd 100%);
+  background-image: linear-gradient(#d9d9d9, #727272 15%, #ffffff 100%);
+`;
+
+const Hair1 = styled.div`
+  position: absolute;
+  transform: rotate(-16deg);
+  border-top-left-radius: 30px;
+  top: 10px;
+  right: 822px;
+  width: 139px;
+  height: 57px;
+  background-color: #010101;
 `;

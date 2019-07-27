4fbe58ac1a959e3425e2cf1627a57a717670d982
Rane <github@blasph.me>
2019-07-26 19:45:23 -0700

    starting face

--- src/canvases/Freddie.js
+++ src/canvases/Freddie.js
@@ -4,11 +4,16 @@ import styled from "styled-components";
 const Freddie = _ => {
   return (
     <Background>
+      {/*  Start hair */}
       <Hair1 />
       <Hair2 />
       <Hair3 />
       <Hair4 />
       <Hair5 />
+      {/*  End hair */}
+      {/*  Start Face */}
+      <Face1 />
+      {/* End Face*/}
     </Background>
   );
 };
@@ -28,10 +33,11 @@ const Hair1 = styled.div`
   transform: rotate(-16deg);
   border-top-left-radius: 30px;
   top: 10px;
-  right: 822px;
+  right: 754px;
   width: 139px;
   height: 57px;
   background-color: #010101;
+  //background-color: pink;
 `;
 
 const Hair2 = styled.div`
@@ -40,9 +46,10 @@ const Hair2 = styled.div`
   transform: rotate(20deg);
   top: 10px;
   height: 104px;
-  width: 171px;
-  right: 744px;
+  width: 200px;
+  right: 655px;
   background-color: #010101;
+  //background-color: pink;
 `;
 
 const Hair3 = styled.div`
@@ -52,29 +59,46 @@ const Hair3 = styled.div`
   top: 60px;
   height: 144px;
   width: 36px;
-  right: 740px;
+  right: 654px;
   background-color: #010101;
+  //background-color: pink;
 `;
 
 const Hair4 = styled.div`
   position: absolute;
   border-top-left-radius: 120%;
   transform: rotate(-5deg);
-  top: 30px;
+  top: 40px;
   height: 144px;
   width: 50px;
-  left: 603px;
+  left: 636px;
   background-color: #010101;
+  //background-color: pink;
 `;
 
 const Hair5 = styled.div`
   position: absolute;
   border-top-left-radius: 130%;
-  border-bottom-right-radius: 90%;
+  //border-bottom-right-radius: 90%;
   transform: rotate(5deg);
   top: 45px;
   height: 144px;
   width: 52px;
-  left: 580px;
+  left: 654px;
   background-color: #010101;
+`;
+
+const Face1 = styled.div`
+  position: absolute;
+  border-top-left-radius: 60%;
+  border-bottom-left-radius: 30%;
+  border-top-right-radius: 30%;
+  border-top-right-radius: -30%;
+  transform: rotate(25deg);
+  height: 107px;
+  width: 210px;
+  top: 80px;
+  right: 670px;
+  background-color: #bbbbbb;
+  //z-index: -1;
 `;
